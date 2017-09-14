e61a09d287a19f26d72325a5913ca46c1814c8e3
esoukhanov <souk0013@algonquinlive.com>
2017-09-14 17:08:36 -0400

    Initial commit. (My 1st git Commit)

---
+++ app/src/main/java/com/example/esoukhanov/androidlab_1/StartAvtivity.java
@@ -0,0 +1,13 @@
+package com.example.esoukhanov.androidlab_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartAvtivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_avtivity);
+    }
+}
